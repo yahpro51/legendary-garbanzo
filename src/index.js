edc850068cdc3584283c47116c114f6e1aa0f595
yahpro51 <khorilov@gmail.com>
2024-10-08 19:37:26 +0900

    push

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div style={{fontSize:100}}></div>
+  </React.StrictMode>
+);
+
